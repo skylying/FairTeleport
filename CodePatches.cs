80ec97f36cb7bbfa867732851beabc3214aff986
Tim Lin <timlin@Tims-MacBook-Pro-2.local>
2024-03-30 09:26:41 +0800

    prepare for coding

--- CodePatches.cs
+++ CodePatches.cs
@@ -11,7 +11,7 @@ namespace MapTeleport
     public partial class ModEntry
     {
         protected static CoordinatesList addedCoordinates;
-        public static bool CheckClickableComponents(List<ClickableComponent> components, int topX, int topY, int x, int y)
+        public static bool CheckClickableComponents(Dictionary<string, ClickableComponent> components, int topX, int topY, int x, int y)
         {
             if (!Config.ModEnabled)
                 return false;
@@ -24,40 +24,66 @@ namespace MapTeleport
             bool added = false;
             bool found = false;
             // Sort boundries so that the function will warp to the smallest overlapping area
-            components.Sort(delegate (ClickableComponent a, ClickableComponent b)
-            {
-                return (a.bounds.Height * a.bounds.Width).CompareTo(b.bounds.Height * b.bounds.Width);
-            });
-            foreach (ClickableComponent c in components)
+            // components.Sort(delegate (ClickableComponent a, ClickableComponent b)
+            // {
+            //     return (a.bounds.Height * a.bounds.Width).CompareTo(b.bounds.Height * b.bounds.Width);
+            // });
+            foreach (var c in components)
             { 
                 // Offset so that this works for any screen size
-                string altId = $"{c.bounds.X - topX}.{c.bounds.Y - topY}";
-                Predicate<Coordinates> findMatch = (o) => o.id == c.myID || (c.myID == ClickableComponent.ID_ignore && o.altId == altId);
-                Coordinates co = coordinates.coordinates.Find(findMatch);
-                if (co == null)
-                {
-                    co = addedCoordinates.coordinates.Find(findMatch);
-                    if (co == null)
-                    {
-                        if (c.myID == ClickableComponent.ID_ignore)
-                        {
-                            addedCoordinates.Add(new Coordinates() { name = c.name, altId = altId, enabled = false });
-                        }
-                        else
-                        {
-                            addedCoordinates.Add(new Coordinates() { name = c.name, id = c.myID, enabled = false });
-                        }
-                        SMonitor.Log($"Added: {{ \"name\":\"{c.name}\", \"id\":{c.myID}, \"altId\":\"{altId}\" }}", LogLevel.Trace);
-                        added = true;
-                    }
-                    // else check if the coordinate is enabled
-                }
+                // string altId = $"{c.Value.bounds.X - topX}.{c.Value.bounds.Y - topY}";
+                // Predicate<Coordinates> findMatch = (o) => o.id == c.Value.myID || (c.Value.myID == ClickableComponent.ID_ignore && o.altId == altId);
+                // Coordinates co = coordinates.coordinates.Find(findMatch);
+                // if (co == null)
+                // {
+                //     co = addedCoordinates.coordinates.Find(findMatch);
+                //     if (co == null)
+                //     {
+                //         if (c.Value.myID == ClickableComponent.ID_ignore)
+                //         {
+                //             addedCoordinates.Add(new Coordinates() { name = c.Value.name, altId = altId, enabled = false });
+                //         }
+                //         else
+                //         {
+                //             addedCoordinates.Add(new Coordinates() { name = c.Value.name, id = c.Value.myID, enabled = false });
+                //         }
+                //         SMonitor.Log($"Added: {{ \"name\":\"{c.Value.name}\", \"id\":{c.Value.myID}, \"altId\":\"{altId}\" }}", LogLevel.Trace);
+                //         added = true;
+                //     }
+                //     // else check if the coordinate is enabled
+                // }
 
-                if (c.containsPoint(x, y) && co.enabled)
+                // if (c.Value.containsPoint(x, y) && co.enabled)
+                // {
+                //     SMonitor.Log($"Teleporting to {c.Value.name} ({(co.altId != null ? co.altId : co.id)}), {co.mapName}, {co.x},{co.y}", LogLevel.Debug);
+                //     Game1.activeClickableMenu?.exitThisMenu(true);
+                //     Game1.warpFarmer(co.mapName, co.x, co.y, false);
+                //     found = true;
+                //     break;
+                // }
+
+                if (c.Value.containsPoint(x, y))
                 {
-                    SMonitor.Log($"Teleporting to {c.name} ({(co.altId != null ? co.altId : co.id)}), {co.mapName}, {co.x},{co.y}", LogLevel.Debug);
-                    Game1.activeClickableMenu?.exitThisMenu(true);
-                    Game1.warpFarmer(co.mapName, co.x, co.y, false);
+                    // Split the input string based on the slash
+                    string[] parts = c.Value.name.Split('/');
+                    string mapName = parts[0];
+                    SMonitor.Log($"Teleport to Map: {mapName}", LogLevel.Debug);
+                    SMonitor.Log($"Location: {c.Value.name}", LogLevel.Debug);
+                    SMonitor.Log($"ID : {c.Value.myID}", LogLevel.Debug);
+
+                    CoordinatesList coordinatesList = SHelper.Data.ReadJsonFile<CoordinatesList>("assets/coordinates.json");
+
+                    //Coordinates destination;
+
+                    foreach (Coordinates coord in coordinatesList.coordinates) {
+                        if (coord.mapName == mapName) {
+                            //Game1.activeClickableMenu?.exitThisMenu(true);
+                            //Game1.warpFarmer(teleportTo, coord.x, coord.y, false);
+                        }
+                    }
+
+                    //Game1.activeClickableMenu?.exitThisMenu(true);
+                    //Game1.warpFarmer(teleportTo, destination.x, co.y, false);
                     found = true;
                     break;
                 }
@@ -91,7 +117,7 @@ namespace MapTeleport
                 if (__instance.allClickableComponents != null && __instance.GetType().Name == "RSVWorldMap")
                 {
                     // RSV uses component x,y's that are not offset, however they need to be offset to check for the mouse position
-                    found = CheckClickableComponents(__instance.allClickableComponents, 0, 0, x - __instance.xPositionOnScreen, y - __instance.yPositionOnScreen);
+                    //found = CheckClickableComponents(__instance.allClickableComponents, 0, 0, x - __instance.xPositionOnScreen, y - __instance.yPositionOnScreen);
                 }
                 return !found;
             }
